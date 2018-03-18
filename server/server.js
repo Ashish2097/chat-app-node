470d8f2a2376109287d68b5d4f3215657981fc43
HOME <ashishduklann@gmail.com>
2018-03-18 15:05:58 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const publicPath = path.join(__dirname+"/../public");
+const port = process.env.PORT || 3000 ;   //port = PORT is specified otherwise, 3000
+app.use(express.static(publicPath));
+
+app.listen('3000',(res, err)=>{
+  if(!err)
+  console.log(`Listening at ${port}`);
+  else
+  console.log("error" + err);
+});
